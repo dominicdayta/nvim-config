5203eba381b8f09322e0d3f8b2167f193897c07f
Dominic Dayta <dbdayta@up.edu.ph>
2026-07-01 10:51:07 +0900

    Adding lualine and gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        -- Configures the gutter styling markers
+        signs = {
+          add          = { text = "┃" },
+          change       = { text = "┃" },
+          delete       = { text = "_" },
+          topdelete    = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked    = { text = "┆" },
+        },
+      })
+    end,
+  }
+}
